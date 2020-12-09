e169d8d28af84eed6e45c03d6cfbb4c526f68d99
PP-Ram <rbabu.bala@gmail.com>
2020-12-09 23:43:02 +0530

    From UK APP

--- Patch/Github_getLastest.ps1
+++ Patch/Github_getLastest.ps1
@@ -5,10 +5,14 @@
 
 Set-Location -Path "C:\SmartForexApp"git config --global user.email rbabu.bala@gmail.comgit config --global user.name PP-Ramif (!(Test-Path -path "C:\SmartForexApp\FxData")) {    git clone https://PP-Ram:pepsiusa10@github.com/PP-Ram/FxData.git} else {   Set-Location -Path "C:\SmartForexApp\FxData"    git pull }
 
+#Copy SmartForexApp patch update 
 Copy-Item "C:\SmartForexApp\FxData\Patch\*.*" "C:\SmartForexApp" -recurse
 
+
+#Open SmartForexApp exe
 Set-Location -Path "C:\SmartForexApp"
 
 & "C:\SmartForexApp\MasterSignal.Exe"
 
+#Kill process
 stop-process -Id $PID
