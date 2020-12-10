e08a9ce1e8c0dcfe7ef23761948d7912ee99ffaf
PP-Ram <rbabu.bala@gmail.com>
2020-12-10 15:12:12 +0530

    From UK APP

--- Patch/Github_CheckIn.ps1
+++
@@ -1,4 +0,0 @@
-﻿
-# update the file into global locationSet-Location -Path "C:\SmartForexApp\FxData"git add .git commit -m "From UK APP"git push
-
-stop-process -Id $PID
--- Patch/Github_getLastest.ps1
+++
@@ -1,18 +0,0 @@
-﻿
-#Read file from global location
-#Create Git folder if not exists - Clone
-#If exists pull
-
-Set-Location -Path "C:\SmartForexApp"git config --global user.email rbabu.bala@gmail.comgit config --global user.name PP-Ramif (!(Test-Path -path "C:\SmartForexApp\FxData")) {    git clone https://PP-Ram:pepsiusa10@github.com/PP-Ram/FxData.git} else {   Set-Location -Path "C:\SmartForexApp\FxData"    git pull }
-
-#Copy SmartForexApp patch update 
-Copy-Item "C:\SmartForexApp\FxData\Patch\*.*" "C:\SmartForexApp" -recurse
-
-
-#Open SmartForexApp exe
-Set-Location -Path "C:\SmartForexApp"
-
-& "C:\SmartForexApp\MasterSignal.Exe"
-
-#Kill process
-stop-process -Id $PID
